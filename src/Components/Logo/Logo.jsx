04efede8e35277c44f0017746f5a267755d936c7
Slade <saib6064@gmail.com>
2025-03-02 18:13:18 +0700

    cleaning unused packages

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { Tilt } from "react-tilt";
 import Brain from "./Brain.jpg";
 const Logo = () => {
   return (
     <div className="ma4 mt0">
-      <Tilt
-        className="Tilt  shadow-2 br2"
-        options={{ max: 55 }}
-        style={{ height: 150, width: 150 }}
-      >
-        <div className="Tilt-inner">
-          <img src={Brain} alt="Brain " className="br3" />
-        </div>
-      </Tilt>
+      <div className="Tilt-inner">
+        <img
+          src={Brain}
+          alt="Brain "
+          className="br3 dim"
+          style={{ maxWidth: "10%", display: "block" }}
+        />
+      </div>
     </div>
   );
 };
