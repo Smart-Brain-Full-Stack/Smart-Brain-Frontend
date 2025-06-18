9c495db7dca382675ddaf62858f4e469a30742c3
Slade <saib6064@gmail.com>
2025-06-19 01:37:24 +0700

    refactor(ProtectedRoute.jsx): remove unnecessary else if statement
    The else if statement was redundant because the return statement
    inside the if block would prevent the else if block from ever
    being executed.

--- src/Routes/ProtectedRoute.jsx
+++ src/Routes/ProtectedRoute.jsx
@@ -4,8 +4,6 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ user, children }) => {
   if (!user) {
     return <Navigate to="/" replace />;
-  } else if (user) {
-    return <Navigate to="/mainpage" replace />;
   }
   return children;
 };
