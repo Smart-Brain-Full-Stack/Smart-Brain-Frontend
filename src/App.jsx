a291342c82d55bb923dfa5786d937917786c255f
Slade <saib6064@gmail.com>
2025-06-19 01:36:51 +0700

    feat(App.jsx): implement face detection using Clarifai API via backend refactor(App.jsx): replace fetch API with axios instance for HTTP requests feat(App.jsx): add error handling for API requests and display user-friendly messages

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import RegisterNav from "./Components/Sign in/RegisterNav";
 import Register from "./Components/Register/Register";
 import PublicRoute from "./Routes/PublicRoute";
 import ProtectedRoute from "./Routes/ProtectedRoute";
+import { axiosinstance } from "../axiosinstance";
 
 function App() {
   const [input, setInput] = useState();
@@ -39,20 +40,14 @@ function App() {
 
   const Req = async () => {
     try {
-      const res = await fetch(
-        " https://stark-ravine-11103-56024eaa1c1d.herokuapp.com/image",
-        {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ id: currUser.id }),
-        }
-      );
+      const { data } = await axiosinstance.put("/image", { id: currUser.id });
 
-      if (!res.ok) {
-        throw new Error(`HTTP error! Status: ${res.status}`);
+      if (!data.entries) {
+        throw new Error(`HTTP error! Status`);
       }
 
-      const { entries } = await res.json();
+      const { entries } = data;
+
       setCurrUser((u) => ({ ...u, entries }));
     } catch (error) {
       console.error(error);
@@ -64,63 +59,52 @@ function App() {
     setInput(e.target.value);
   };
 
-  const onSubmit = function () {
+  const onSubmit = async () => {
     if (currUser) {
       setImgUrl(input);
 
-      fetch(
-        "https://stark-ravine-11103-56024eaa1c1d.herokuapp.com/detect-face",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            imageUrl: input,
-          }),
+      try {
+        const { data } = await axiosinstance.post("/detect-face", {
+          imageUrl: input,
+        });
+
+        if (data.status.description !== "Ok") {
+          throw new Error(`HTTP error!`);
         }
-      )
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${res.status}`);
-          }
-          //Updating entries
 
-          return response.json();
-        })
-        .then((result) => {
-          const regions = result.outputs[0].data.regions;
-          if (regions) {
-            Req();
-          }
-          //assign
-          let topRow, leftCol, bottomRow, rightCol;
-          let tempBox = [];
+        const regions = data.outputs[0].data.regions;
+        if (regions) {
+          Req();
+        }
+        //assign
+        let topRow, leftCol, bottomRow, rightCol;
+        let tempBox = [];
 
-          const image = document.getElementById("inputImage");
-          const width = Number(image.width);
-          const height = Number(image.height);
+        const image = document.getElementById("inputImage");
+        const width = Number(image.width);
+        const height = Number(image.height);
 
-          regions.forEach((region) => {
-            // Accessing and rounding the bounding box values
-            const boundingBox = region.region_info.bounding_box;
-            topRow = boundingBox.top_row * height;
-            leftCol = boundingBox.left_col * width;
-            bottomRow = height - boundingBox.bottom_row * height;
-            rightCol = width - boundingBox.right_col * width;
+        regions.forEach((region) => {
+          // Accessing and rounding the bounding box values
+          const boundingBox = region.region_info.bounding_box;
+          topRow = boundingBox.top_row * height;
+          leftCol = boundingBox.left_col * width;
+          bottomRow = height - boundingBox.bottom_row * height;
+          rightCol = width - boundingBox.right_col * width;
 
-            //box object
-            const box = {
-              topRow: topRow,
-              leftCol: leftCol,
-              bottomRow: bottomRow,
-              rightCol: rightCol,
-            };
-            tempBox.push(box);
-          });
-          setBoxes(tempBox);
-        })
-        .catch((error) => console.log("Error:", error));
-    } else {
-      alert("You need to login first!!!");
+          //box object
+          const box = {
+            topRow: topRow,
+            leftCol: leftCol,
+            bottomRow: bottomRow,
+            rightCol: rightCol,
+          };
+          tempBox.push(box);
+        });
+        setBoxes(tempBox);
+      } catch (error) {
+        console.log("Error:", error);
+      }
     }
   };
 
